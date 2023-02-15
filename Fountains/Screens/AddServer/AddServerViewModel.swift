a9ba56e4892151e6596ad4efcda2d733c5ebadb1
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-02-15 11:23:33 +0100

    wip: call kotlin from swift

--- Fountains/Screens/AddServer/AddServerViewModel.swift
+++ Fountains/Screens/AddServer/AddServerViewModel.swift
@@ -1,6 +1,7 @@
 import DomainLayer
 import Foundation
 import MapKit
+import WaterFountains
 
 final class AddServerViewModel: ObservableObject {
     private let getDiscoveredUseCase = GetDiscoveredServersUseCase()
@@ -8,9 +9,9 @@ final class AddServerViewModel: ObservableObject {
     private let addServerUseCase = CreateServerUseCase()
 
     @Published var isLoading: Bool = false
-    @Published var discoveredServers: [ServerDiscoveryItem] = []
+    @Published var discoveredServers: [ServerDiscoveryItemDto] = []
     @Published var address: String = ""
-    @Published var serverInfo: ServerInfo?
+    @Published var serverInfo: ServerInfoDto?
     var previewMapRegion: MKCoordinateRegion {
         guard let serverInfo else { return MKCoordinateRegion() }
         return MKCoordinateRegion(
@@ -25,28 +26,52 @@ final class AddServerViewModel: ObservableObject {
     @MainActor
     public func getDiscoveredServers() async {
         isLoading = true
-        await discoveredServers = getDiscoveredUseCase.execute()
+        discoveredServers = await getDiscoveredUseCase.execute()
         isLoading = false
     }
 
     @MainActor
     public func getServerInfo() async {
-        guard let url = URL(string: address) else { return }
+//        guard let url = URL(string: address) else { return }
         isLoading = true
-        serverInfo = await getInfoUseCase.execute(url: url)
+        serverInfo = await getInfoUseCase.execute(url: address)
         isLoading = false
     }
 
     @MainActor
-    public func checkPredefinedServer(at address: URL) async {
-        self.address = address.absoluteString
+    public func checkPredefinedServer(at address: String) async {
+        self.address = address
         await getServerInfo()
     }
 
     public func addServer(callback: () -> Void) {
-        guard let area = serverInfo?.area, let address = URL(string: address) else { return }
-        let server = Server(name: area.displayName, address: address, location: area.location)
-        addServerUseCase.execute(server: server)
+//        guard let area = serverInfo?.area, let address = URL(string: address) else { return }
+//        let server = Server(name: area.displayName, address: address, location: area.location)
+//        addServerUseCase.execute(server: server)
         callback()
+    }
+}
+
+struct GetDiscoveredServersUseCase {
+    private let dataSource = DiscoveryDataSource()
+
+    @MainActor
+    func execute() async -> [ServerDiscoveryItemDto] {
+        try! await dataSource.all()
+    }
+}
+
+struct GetServerInfoUseCase {
+    private let dataSource = ServerInfoDataSource()
+
+    @MainActor
+    func execute(url: String) async -> ServerInfoDto? {
+        try? await dataSource.get(baseUrl: url)
+    }
+}
+
+extension LocationDto {
+    public var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
 }
