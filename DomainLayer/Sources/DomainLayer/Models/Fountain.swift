b5f834419eac54e0b4767d802d2348dd76c5c67b
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-03-31 17:55:09 +0200

    feat: restyle fountain detail

--- DomainLayer/Sources/DomainLayer/Models/Fountain.swift
+++ DomainLayer/Sources/DomainLayer/Models/Fountain.swift
@@ -14,6 +14,7 @@ extension Fountain {
         public let bottle: Value
         public let wheelchair: Value
         public let mapillaryId: String?
+        public let checkDate: Date?
     }
 }
 
