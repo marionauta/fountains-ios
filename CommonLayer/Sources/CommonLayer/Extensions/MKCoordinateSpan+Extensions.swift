25a9082714939790a633064c7ee3c4171430fe9d
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-04-03 17:26:01 +0200

    feat: use map without clustering

--- CommonLayer/Sources/CommonLayer/Extensions/MKCoordinateSpan+Extensions.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import MapKit
-
-extension MKCoordinateSpan: Equatable {
-    public static func == (lhs: MKCoordinateSpan, rhs: MKCoordinateSpan) -> Bool {
-        return lhs.latitudeDelta == rhs.latitudeDelta
-            && lhs.longitudeDelta == rhs.longitudeDelta
-    }
-}
