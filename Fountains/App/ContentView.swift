050fee3167abf96910ab01dd54857dec61c9a0e9
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-04-19 15:14:05 +0200

    feat: add app name behind notch

--- Fountains/App/ContentView.swift
+++ Fountains/App/ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         MapCoordinator()
+            .withNotchBrand("app_name", backgroundColor: .accentColor)
             .withAdmob()
             .withPulse()
     }
