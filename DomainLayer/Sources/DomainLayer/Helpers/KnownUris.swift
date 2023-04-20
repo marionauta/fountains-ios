432f6bf150d1e246ef359bde6735636be68e0db6
Mario Nachbaur <marionauta@users.noreply.github.com>
2023-04-20 10:46:45 +0200

    fix: referrer

--- DomainLayer/Sources/DomainLayer/Helpers/KnownUris.swift
+++ DomainLayer/Sources/DomainLayer/Helpers/KnownUris.swift
@@ -2,7 +2,7 @@ import Foundation
 import WaterFountains
 
 public enum KnownUris {
-    public static let website = URL(string: WaterFountains.KnownUris.companion.website)!
+    public static let website = URL(string: WaterFountains.KnownUris.companion.website())!
     public static let developer = URL(string: WaterFountains.KnownUris.companion.developer)!
 
     public static func help(slug: String) -> URL {
