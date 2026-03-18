ebcae3aa08da713c3f7a65be2092505c4943435e
Mario Nachbaur <noreply@nauta.one>
2026-03-18 19:27:46 +0100

    cleanup before swift export

--- Fountains/Screens/Map/MapViewModel.swift
+++ Fountains/Screens/Map/MapViewModel.swift
@@ -27,7 +27,7 @@ final class MapViewModel: NSObject, ObservableObject {
     @Published public var route: MapCoordinator.Route?
 
     private let getAmenities = GetAmenitiesUseCase(
-        languages: GetLanguagesUseCase.shared(),
+        languages: GetLanguagesUseCase.shared.invoke(),
         amenityRepository: .shared,
         settingsRepository: FilterSettingsRepositoryImpl(),
     )
