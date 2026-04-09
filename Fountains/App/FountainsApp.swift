7de3276e14290a50e3e1d83ecd0e74573699c8a2
Mario Nachbaur <noreply@nauta.one>
2026-04-09 14:36:02 +0200

    refresh feature flags

--- Fountains/App/FountainsApp.swift
+++ Fountains/App/FountainsApp.swift
@@ -1,13 +1,27 @@
 import SwiftUI
+import OpenLocationsShared
 
 @main
 struct FountainsApp: App {
     @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .modifier(PurchaseManagerModifier())
+                .task(id: scenePhase) { [scenePhase] in
+                    guard scenePhase == .active else { return }
+                    await reloadFeatureFlags()
+                }
+        }
+    }
+
+    private func reloadFeatureFlags() async {
+        do {
+            try await RefreshFeatureFlagsUseCase.shared()
+        } catch {
+            print("\(#function) error: \(error)")
         }
     }
 }
